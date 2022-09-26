b3605601c1b31203b7e90ba8cf113f469f396456
pablogarciamoralez <pablogarciamoralez@gmail.com>
2022-09-26 12:28:02 -0300

    stdin stdout

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const { mostrarMenu, pausa } = require('./helpers/messages')
 console.clear()
 
 const main = async() => {
-    mostrarMenu()
-    pausa()
+    let opt = ''
+    do {
+        opt = await mostrarMenu()
+        console.log({opt})
+        if (opt !== '0') await pausa()
+    }
+
+    while (
+        opt !== '0'
+    )
 }
 
 main();
