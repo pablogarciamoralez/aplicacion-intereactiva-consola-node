817e1ea70b4b43470d72a1151f813e5838640cc3
pablogarciamoralez <pablogarciamoralez@gmail.com>
2022-09-27 11:40:49 -0300

    creacion y listado de tareas

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('colors')
-const { inquirerMenu, pausa } = require('./helpers/inquirer')
+const { inquirerMenu, pausa, leerInput } = require('./helpers/inquirer')
+const Tareas = require('./models/tareas')
 // const { pausa } = require('./helpers/messages')
 // const { mostrarMenu, pausa } = require('./helpers/messages')
 
@@ -7,10 +8,22 @@ console.clear()
 
 const main = async() => {
     let opt = ''
+    const tareas = new Tareas
     do {
         opt = await inquirerMenu()
+        
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripcion: ')
+                tareas.crearTarea( desc )
+                console.log(desc)
+            break
+
+            case '2': 
+            console.log(tareas._listado)
+        }
+
         await pausa()
-        // if (opt !== '0') await pausa()
     }
 
     while (
